fd7a6bef2b59449a072bcb8e88c04b748a160e66
Facundo Aletto <facundoaletto95@gmail.com>
2023-03-27 01:28:14 -0300

    first commit

---
+++ algoritmo1-4.js
@@ -0,0 +1,23 @@
+document.querySelector("body").innerHTML = `
+<h1>La selección se prepara para su segundo amistoso</h1>
+    
+    <h2>Los campeones del mundo se enfrentan este martes contra Curazao</h2>
+
+    <p>Aquí encontrará todos los detalles del encuentro</p>
+
+    <details>
+        <summary>Información detallada del partido</summary>
+
+        <p>Al desplegar esta sección encontra detalles sobre horario y lugar donde se desarrollara el evento, y un acceso directo al sitio web para la venta oficial de las entradas</p>
+
+        <img src="https://www.elliberal.com.ar/fotos/notas/2023/03/22/625308_20230322164214.jpg" alt="Arg vs. Curazao">
+
+        <p>Hora y lugar: 
+            Martes 28 20:30 hs
+            Estadio Único Madre de Ciudades, Santiago del Estero</p>
+
+        <a href="https://www.deportick.com/">Click aquí para conseguir tu entrada</a>
+
+        <p>Si ya cuenta con su entrada, aquí debajo puede encontrar el cronograma de retiro</p>
+   <iframe height="400" width="720" src="https://www.deportick.com/puntos-de-entrega" frameborder="0"></iframe>
+    </details>`;
